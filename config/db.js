e6fe2005a727d19b51e9bf9f8e01de99cee36606
fhx10012091 <1875934025@qq.com>
2021-07-18 17:59:39 +0800

    完成获取模板信息接口

---
+++ config/db.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/** MONGODB */
+const mongodbUrl = 'mongodb://127.0.0.1:27017/starfish-cli';
+const mongodbDbName = 'starfish-cli';
+
+module.exports = {
+  mongodbDbName,
+  mongodbUrl,
+};
